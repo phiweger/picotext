700de8ad1c3a7feb79b7334cc067c049ab1cbffd
phiweger <3918306+phiweger@users.noreply.github.com>
2020-05-20 12:41:35 +0200

    NN seems to train successfully

--- sketches.py
+++ sketches.py
@@ -1,20 +1,12 @@
-import screed
-from tqdm import tqdm
+import time
+import math
+import os
+import torch
+import torch.nn as nn
+import torch.onnx
 
-from tokenizers import CharBPETokenizer
-
-from utils import load_pretrained_tokenizer
-
-
-tokenizer = load_pretrained_tokenizer(
-    CharBPETokenizer, './uniprot_sprot.dayhoff')
-
-numbers = []
-with open('uniprot_sprot.dayhoff.txt', 'r') as file:
-    for line in file:
-        encoded = tokenizer.encode(line.strip())
-        tokens, ids = encoded.tokens, encoded.ids
-
+from picotext.data import Corpus
+from picotext.model import RNNModel
 
 
 '''
@@ -33,9 +25,6 @@ with open('uniprot_sprot.dayhoff.txt', 'r') as file:
 !head -n 2000 uniprot_sprot.dayhoff.txt | tail -n 1000 > valid.txt
 '''
 
-corpus = Corpus('.')  # TODO: tokenization happens here
-device = 'cpu'
-
 
 def batchify(data, bsz):
     # Work out how cleanly we can divide the dataset into bsz parts.
@@ -47,10 +36,141 @@ def batchify(data, bsz):
     return data.to(device)
 
 
+def repackage_hidden(h):
+    """Wraps hidden states in new Tensors, to detach them from their history."""
+
+    if isinstance(h, torch.Tensor):
+        return h.detach()
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+
+def get_batch(source, i):
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i:i+seq_len]
+    target = source[i+1:i+1+seq_len].view(-1)
+    return data, target
+
+
+def evaluate(data_source):
+    # Turn on evaluation mode which disables dropout.
+    model.eval()
+    total_loss = 0.
+    ntokens = corpus.tokenizer.get_vocab_size()
+    # if args.model != 'Transformer':
+    
+    hidden = model.init_hidden(eval_batch_size)
+    with torch.no_grad():
+        for i in range(0, data_source.size(0) - 1, bptt):
+            data, targets = get_batch(data_source, i)
+            # if args.model == 'Transformer':
+            #     output = model(data)
+            #     output = output.view(-1, ntokens)
+            # else:
+            output, hidden = model(data, hidden)
+            hidden = repackage_hidden(hidden)
+            total_loss += len(data) * criterion(output, targets).item()
+    return total_loss / (len(data_source) - 1)
+
+
+def train():
+    # Turn on training mode which enables dropout.
+    model.train()
+    total_loss = 0.
+    start_time = time.time()
+    ntokens = corpus.tokenizer.get_vocab_size()  # len(corpus.dictionary)
+    # if args.model != 'Transformer':
+    hidden = model.init_hidden(batch_size)
+    
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
+        data, targets = get_batch(train_data, i)
+        # Starting each batch, we detach the hidden state from how it was previously produced.
+        # If we didn't, the model would try backpropagating all the way to start of the dataset.
+        model.zero_grad()
+        # if args.model == 'Transformer':
+        #     output = model(data)
+        #     output = output.view(-1, ntokens)
+        # else:
+        hidden = repackage_hidden(hidden)
+        output, hidden = model(data, hidden)
+        
+        loss = criterion(output, targets)
+        loss.backward()
+
+        # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        for p in model.parameters():
+            p.data.add_(-lr, p.grad.data)
+
+        total_loss += loss.item()
+
+        if batch % log_interval == 0 and batch > 0:
+            cur_loss = total_loss / log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
+                    'loss {:5.2f} | ppl {:8.2f}'.format(
+                epoch, batch, len(train_data) // bptt, lr,
+                elapsed * 1000 / log_interval, cur_loss, math.exp(cur_loss)))
+            total_loss = 0
+            start_time = time.time()
+
+
+device = 'cpu'
+# preprocessing
+batch_size = 20
 eval_batch_size = 10
-train_data = batchify(corpus.train, 5)
+# model
+model_type = 'GRU'  # LSTM
+ntokens = corpus.tokenizer.get_vocab_size()
+emsize = 200
+nhid = 200
+nlayers = 2
+dropout = 0.2
+tied = False
+# training
+lr = 20
+best_val_loss = None
+epochs = 10
+bptt = 35
+clip = 0.25
+log_interval = 200
+save = 'final'
 
 
+corpus = Corpus(
+    sequences='uniprot_sprot.fasta.gz',
+    tokenizer='uniprot_sprot.dayhoff',
+    split=[0.8, 0.1, 0.1], seed=42)
 
+train_data = batchify(corpus.train, batch_size)
+val_data = batchify(corpus.dev, eval_batch_size)
+test_data = batchify(corpus.test, eval_batch_size)
 
+model = RNNModel(
+    model_type, ntokens, emsize, nhid, nlayers, dropout, tied).to(device)
+
+criterion = nn.NLLLoss()
+
+# At any point you can hit Ctrl + C to break out of training early.
+try:
+    for epoch in range(1, epochs+1):
+        epoch_start_time = time.time()
+        train()
+        val_loss = evaluate(val_data)
+        print('-' * 89)
+        print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+                'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                           val_loss, math.exp(val_loss)))
+        print('-' * 89)
+        # Save the model if the validation loss is the best we've seen so far.
+        if not best_val_loss or val_loss < best_val_loss:
+            with open(save, 'wb') as f:
+                torch.save(model, f)
+            best_val_loss = val_loss
+        else:
+            # Anneal the learning rate if no improvement has been seen in the validation dataset.
+            lr /= 4.0
+except KeyboardInterrupt:
+    print('-' * 89)
+    print('Exiting from training early')
 
