6bf1a3a0da614c243869b86dd20dc0e2cd6c08dd
phiweger <3918306+phiweger@users.noreply.github.com>
2020-05-21 07:34:58 +0200

    runs, now optimize

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ from picotext.model import RNNModel
 
 
 def batchify(data, bsz):
+    '''
+    https://github.com/pytorch/examples/blob/master/word_language_model/main.py#L68
+    '''
     # Work out how cleanly we can divide the dataset into bsz parts.
     nbatch = data.size(0) // bsz
     # Trim off any extra elements that wouldn't cleanly fit (remainders).
@@ -37,8 +40,9 @@ def batchify(data, bsz):
 
 
 def repackage_hidden(h):
-    """Wraps hidden states in new Tensors, to detach them from their history."""
-
+    '''
+    Wraps hidden states in new Tensors, to detach them from their history.
+    '''
     if isinstance(h, torch.Tensor):
         return h.detach()
     else:
@@ -46,9 +50,15 @@ def repackage_hidden(h):
 
 
 def get_batch(source, i):
+    '''
+    https://github.com/pytorch/examples/blob/master/word_language_model/main.py#L119
+    '''
     seq_len = min(bptt, len(source) - 1 - i)
     data = source[i:i+seq_len]
     target = source[i+1:i+1+seq_len].view(-1)
+    # .view(-1) concatenates the target vectors bc/ downstream this is what the
+    # criterion expects (the output of the model is of equal dimensions, albeit
+    # its input is still batched up when it leaves the get_batch() fn.
     return data, target
 
 
@@ -73,6 +83,15 @@ def evaluate(data_source):
     return total_loss / (len(data_source) - 1)
 
 
+def accuracy_score(y_true, y_pred):
+    '''
+    stackoverflow.com/questions/43962599
+    '''
+    y_pred = np.concatenate(tuple(y_pred))
+    y_true = np.concatenate(tuple([[t for t in y] for y in y_true])).reshape(y_pred.shape)
+    return (y_true == y_pred).sum() / float(len(y_true))
+
+
 def train():
     # Turn on training mode which enables dropout.
     model.train()
@@ -94,7 +113,7 @@ def train():
         hidden = repackage_hidden(hidden)
         output, hidden = model(data, hidden)
         
-        loss = criterion(output, targets)
+        loss = criterion(output, targets)  # y_pred, y_true
         loss.backward()
 
         # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
@@ -107,6 +126,7 @@ def train():
         if batch % log_interval == 0 and batch > 0:
             cur_loss = total_loss / log_interval
             elapsed = time.time() - start_time
+                        
             print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | ms/batch {:5.2f} | '
                     'loss {:5.2f} | ppl {:8.2f}'.format(
                 epoch, batch, len(train_data) // bptt, lr,
@@ -115,9 +135,16 @@ def train():
             start_time = time.time()
 
 
+# Let's go
+corpus = Corpus(
+    sequences='uniprot_sprot.fasta.gz',
+    tokenizer='uniprot_sprot.dayhoff',
+    split=[0.8, 0.1, 0.1], seed=42)
+
+
 device = 'cpu'
 # preprocessing
-batch_size = 20
+batch_size = 20  # TODO: todal seq len bptt * batch_size?
 eval_batch_size = 10
 # model
 model_type = 'GRU'  # LSTM
@@ -130,17 +157,12 @@ tied = False
 # training
 lr = 20
 best_val_loss = None
-epochs = 10
+epochs = 1
 bptt = 35
 clip = 0.25
 log_interval = 200
 save = 'final'
 
-
-corpus = Corpus(
-    sequences='uniprot_sprot.fasta.gz',
-    tokenizer='uniprot_sprot.dayhoff',
-    split=[0.8, 0.1, 0.1], seed=42)
 
 train_data = batchify(corpus.train, batch_size)
 val_data = batchify(corpus.dev, eval_batch_size)
@@ -173,4 +195,37 @@ try:
 except KeyboardInterrupt:
     print('-' * 89)
     print('Exiting from training early')
+
+
+
+'''
+https://discuss.pytorch.org/t/got-warning-couldnt-retrieve-source-code-for-container/7689/5
+
+| epoch   1 | 48400/48492 batches | lr 20.00 | ms/batch 205.63 | loss  7.93 | ppl  2766.47
+-----------------------------------------------------------------------------------------
+| end of epoch   1 | time: 10171.76s | valid loss  7.84 | valid ppl  2545.27
+-----------------------------------------------------------------------------------------
+/Users/phi/miniconda3/envs/redux/lib/python3.7/site-packages/torch/serialization.py:360: UserWarning: Couldn't retrieve source code for container of type RNNModel. It won't be checked for correctness upon loading.
+  "type " + obj.__name__ + ". It won't be checked "
+| epoch   2 |   200/48492 batches | lr 20.00 | ms/batch 210.18 | loss  7.92 | ppl  2756.23
+'''
+
+
+# TODO: accuracy
+# https://discuss.pytorch.org/t/training-accuracy-do-not-increase-when-train-lstm-with-batchsize-1/7578
+# --> https://www.analyticsvidhya.com/blog/2020/01/first-text-classification-in-pytorch/
+
+# TODO: optimizer, where?
+# https://machinetalk.org/2019/02/08/text-generation-with-pytorch/
+# https://www.analyticsvidhya.com/blog/2020/01/first-text-classification-in-pytorch/
+
+# TODO: modify
+# https://github.com/fastai/course-v3/blob/master/nbs/dl2/12_text.ipynb
+
+# TODO; LSTM explained
+# http://colah.github.io/posts/2015-08-Understanding-LSTMs/
+
+# TODO: deploy on Floydhub
+
+# TODO: bptt lower
 
